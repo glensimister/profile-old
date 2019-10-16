5e3f5b0e47b8bb3eef7749433128123b6767a7e8
Glen Simister <glen_simister@yahoo.co.uk>
2019-10-16 10:10:30 +0200

    refactoring

--- js/api.js
+++
@@ -1,8 +0,0 @@
-export function readProfile() {
-    const endpoint = 'sampleData/profile.json';
-    return new Promise(resolve => {
-        $.getJSON(endpoint, function (res) {
-            resolve(res);
-        });
-    });
-}
